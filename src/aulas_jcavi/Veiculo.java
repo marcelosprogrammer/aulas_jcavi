9bbf49bb9402b7c642f9618bd7edce3ae4d95a1e
marcelosprogrammer <umadosedeprogramacao@gmail.com>
2020-10-16 11:23:01 -0300

    inclusão do construtor e dos getters e setters

--- src/aulas_jcavi/Veiculo.java
+++ src/aulas_jcavi/Veiculo.java
@@ -13,4 +13,27 @@ public class Veiculo {
 	private String nome;
 	private String marca;
 	
+	public Veiculo() {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
+	
+	
 }
