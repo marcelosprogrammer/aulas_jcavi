6d9b9257058465e16a5a3b5825dc93fa1e5a59a8
marcelosprogrammer <umadosedeprogramacao@gmail.com>
2020-10-16 11:20:41 -0300

    Classe Veiculo adicionada ao projeto

---
+++ src/aulas_jcavi/Veiculo.java
@@ -0,0 +1,16 @@
+package aulas_jcavi;
+
+/**
+ * @author Marcelo da Silva
+ * Esta classe utiliza de alguns atributos que naturalmente dcevem ser criados como um Objeto,
+ * exemplo marca, deveriamos criar uma Classe Marca e após isto sim fazer a criação de um objeto do tipo Marca para compor o veículo
+ * para fins didáticos esta sendo definido desta forma, em versões futuras será alterado para o correto.
+ * 
+ */
+
+public class Veiculo {
+
+	private String nome;
+	private String marca;
+	
+}
